2cbf7d288ffbb02e350797e3c19fd24943c34685
CloudYin <yzhyff@163.com>
2020-02-10 14:51:37 +0800

    update code

--- scripts/psen_scan.py
+++ scripts/psen_scan.py
@@ -5,27 +5,28 @@ from std_srvs.srv import Trigger
 import rospy
 import math
 
+
+pause_trigger = rospy.ServiceProxy('move_path_B', Trigger)
+resume_trigger = rospy.ServiceProxy('continue_move_path_A', Trigger)
+
 def laser_scan_callback(msg):
     # In angle between 0 to 90 degree
     # If distance < 0.5m, call "move_path_B" service
     # Else, call "continue_move_path_A" service
-    for index in range(0, 900):
-        if msg.ranges[index] < 0.5:
-            rospy.wait_for_service('move_path_B', Trigger)
-            try:
-                pause_trigger = rospy.ServiceProxy('move_path_B', Trigger)
-                pause_trigger_response = pause_trigger()
-                print(pause_trigger_response.message)
-            except rospy.ServiceException:
-                print("Service move_path_B call failed") 
-        else:
-            rospy.wait_for_service('continue_move_path_A', Trigger)
-            try:
-                resume_trigger = rospy.ServiceProxy('continue_move_path_A', Trigger)
-                resume_trigger_response = resume_trigger()
-                print(resume_trigger_response.message)
-            except rospy.ServiceException:
-                print("Service continue_move_path_A call failed") 
+    if min(msg.ranges) < 0.5:
+        print("Zone intruded")
+        try:
+            pause_trigger_response = pause_trigger()
+            print("move path B trigger: " + str(pause_trigger_response.message))
+        except rospy.ServiceException:
+            print("Service move_path_B call failed") 
+    elif min(msg.ranges) >= 1:
+        print("Zone not intruded")
+        try:
+            resume_trigger_response = resume_trigger()
+            print("move path A trigger: " + str(resume_trigger_response.message))
+        except rospy.ServiceException:
+            print("Service continue_move_path_A call failed")
 
 
 if __name__ == "__main__":
